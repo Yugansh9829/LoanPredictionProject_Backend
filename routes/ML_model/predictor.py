b8739fee78a03638eb0a0bc78beee03e8dbc3396
yugansh soni <yuganshsoni8@gmail.com>
2024-01-18 08:29:35 +0530

    adding virtual environment

--- routes/ML_model/predictor.py
+++ routes/ML_model/predictor.py
@@ -1,10 +1,6 @@
 import pickle
 import sys
-import sklearn
 import json
-
-print("Python Version:", sys.version)
-print("scikit-learn Version:", sklearn.__version__)
 
 def load_model(model_path):
     with open(model_path, 'rb') as file:
